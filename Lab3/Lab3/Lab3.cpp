2e9dcc97e40408b1f9d71e657e1d58c9a2db6cde
Drawishe <fogreen7@gmail.com>
2022-06-05 01:34:36 +0300

    lab3 beta2

--- Lab3/Lab3/Lab3.cpp
+++ Lab3/Lab3/Lab3.cpp
@@ -2,6 +2,10 @@
 #include <tchar.h>
 #include <string.h>
 
+#include "sgx_urts.h"
+#include "sgx_tseal.h"
+#include "Lab3_Enclave_u.h"
+#define ENCLAVE_FILE _T ("Lab3_Enclave.signed.dll")
 #define BUF_LEN 100 // Длина буфера обмена между анклавом и небезопасным приложением
 /*
 const char table[6][41] = {
@@ -29,6 +33,18 @@ int main() {
 
 	char buffer[BUF_LEN] = { 0 };
 
+	sgx_enclave_id_t eid;
+	sgx_status_t ret = SGX_SUCCESS;
+	sgx_launch_token_t token = { 0 };
+	int updated = 0;
+
+	ret = sgx_create_enclave(ENCLAVE_FILE, SGX_DEBUG_FLAG, &token, &updated, &eid, NULL);
+	if (ret != SGX_SUCCESS) {
+		printf("Application error %#x, failed to create enclave.\n", ret);
+		return -1;
+	}
+
+
 	while (true) {
 		printf("Enter a number of object to get information or enter -1 to exit a program: \t");
 		int idx = -1;
@@ -36,8 +52,10 @@ int main() {
 		if (idx < 0) {
 			return 0;
 		}
-		foo(buffer, BUF_LEN, idx);
+		foo(eid, buffer, BUF_LEN, idx);
 		printf("%s \n******************************************\n\n", buffer);
 	}
+	if (SGX_SUCCESS != sgx_destroy_enclave(eid))
+		return -1;
 	return 0;
 }
